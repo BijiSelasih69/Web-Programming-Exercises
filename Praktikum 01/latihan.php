2513652fa9b10d5a6f56d060ef1d59b0187766bb
BijiSelasih69 <57758179+BijiSelasih69@users.noreply.github.com>
2021-03-19 20:20:57 +0700

    Add files via upload

---
+++ Praktikum 01/latihan.php
@@ -0,0 +1,11 @@
+<html>
+	<head><center><h1>SELAMAT DATANG<h1></h1><br><<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>><br><br></center></head>
+	<body>
+		<?php
+			echo "<h1><center>OTNIEL DEWANGGA DIVAN PRAMUDYA</center></h1>";
+			echo "<h2><center>K3519065</center></h2>";
+			echo "<h3><center>PTIK 2019</center></h3>";
+			echo "<br><br><center>Work In Progress</center>";
+		?>
+	</body>
+</html>

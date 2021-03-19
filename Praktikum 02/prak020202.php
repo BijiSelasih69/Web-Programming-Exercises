ba14712120d7923be330d6e519eb9100d68ddb5e
BijiSelasih69 <57758179+BijiSelasih69@users.noreply.github.com>
2021-03-19 22:47:35 +0700

    Add files via upload

---
+++ Praktikum 02/prak020202.php
@@ -0,0 +1,17 @@
+<?php
+	$baris = 4;
+	$kolom = 5;
+	$angka = 1;
+	
+	echo "<table border='1'>";
+	for ($i=1; $i <= $baris; $i++) { 
+		echo "<tr>";
+			
+		for ($j=1; $j <= $kolom; $j++) { 
+			echo "<td>$angka</td>";
+		$angka += 1;
+		}
+		echo "</tr>";
+	}
+	echo "</table>";
+?>
